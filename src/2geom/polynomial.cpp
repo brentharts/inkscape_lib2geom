afa12b11de1f1c381e5c06b0def3e810eecd92e6
Rafael Siejakowski <rs@rs-math.net>
2023-08-12 12:21:46 +0200

    Clean up polynomial.cpp
    Remove trailing whitespace and commented out code.

--- src/2geom/polynomial.cpp
+++ src/2geom/polynomial.cpp
@@ -49,9 +49,9 @@ namespace Geom {
 #endif
 
 Poly Poly::operator*(const Poly& p) const {
-    Poly result; 
+    Poly result;
     result.resize(degree() +  p.degree()+1);
-    
+
     for(unsigned i = 0; i < size(); i++) {
         for(unsigned j = 0; j < p.size(); j++) {
             result[i+j] += (*this)[i] * p[j];
@@ -60,10 +60,6 @@ Poly Poly::operator*(const Poly& p) const {
     return result;
 }
 
-/*double Poly::eval(double x) const {
-    return gsl_poly_eval(&coeff[0], size(), x);
-    }*/
-
 void Poly::normalize() {
     while(back() == 0)
         pop_back();
@@ -71,9 +67,9 @@ void Poly::normalize() {
 
 void Poly::monicify() {
     normalize();
-    
+
     double scale = 1./back(); // unitize
-    
+
     for(unsigned i = 0; i < size(); i++) {
         (*this)[i] *= scale;
     }
@@ -84,25 +80,23 @@ void Poly::monicify() {
 std::vector<std::complex<double> > solve(Poly const & pp) {
     Poly p(pp);
     p.normalize();
-    gsl_poly_complex_workspace * w 
+    gsl_poly_complex_workspace * w
         = gsl_poly_complex_workspace_alloc (p.size());
-       
+
     gsl_complex_packed_ptr z = new double[p.degree()*2];
     double* a = new double[p.size()];
     for(unsigned int i = 0; i < p.size(); i++)
         a[i] = p[i];
     std::vector<std::complex<double> > roots;
-    //roots.resize(p.degree());
-    
+
     gsl_poly_complex_solve (a, p.size(), w, z);
     delete[]a;
-     
+
     gsl_poly_complex_workspace_free (w);
-     
+
     for (unsigned int i = 0; i < p.degree(); i++) {
         roots.emplace_back(z[2*i] ,z[2*i+1]);
-        //printf ("z%d = %+.18f %+.18f\n", i, z[2*i], z[2*i+1]);
-    }    
+    }
     delete[] z;
     return roots;
 }
@@ -110,7 +104,7 @@ std::vector<std::complex<double> > solve(Poly const & pp) {
 std::vector<double > solve_reals(Poly const & p) {
     std::vector<std::complex<double> > roots = solve(p);
     std::vector<double> real_roots;
-    
+
     for(auto & root : roots) {
         if(root.imag() == 0) // should be more lenient perhaps
             real_roots.push_back(root.real());
@@ -121,7 +115,7 @@ std::vector<double > solve_reals(Poly const & p) {
 
 double polish_root(Poly const & p, double guess, double tol) {
     Poly dp = derivative(p);
-    
+
     double fn = p(guess);
     while(fabs(fn) > tol) {
         guess -= fn/dp(guess);
@@ -132,7 +126,7 @@ double polish_root(Poly const & p, double guess, double tol) {
 
 Poly integral(Poly const & p) {
     Poly result;
-    
+
     result.reserve(p.size()+1);
     result.push_back(0); // arbitrary const
     for(unsigned i = 0; i < p.size(); i++) {
@@ -144,7 +138,7 @@ Poly integral(Poly const & p) {
 
 Poly derivative(Poly const & p) {
     Poly result;
-    
+
     if(p.size() <= 1)
         return Poly(0);
     result.reserve(p.size()-1);
@@ -156,58 +150,34 @@ Poly derivative(Poly const & p) {
 
 Poly compose(Poly const & a, Poly const & b) {
     Poly result;
-    
+
     for(unsigned i = a.size(); i > 0; i--) {
         result = Poly(a[i-1]) + result * b;
     }
     return result;
-    
-}
 
-/* This version is backwards - dividing taylor terms
-Poly divide(Poly const &a, Poly const &b, Poly &r) {
-    Poly c;
-    r = a; // remainder
-    
-    const unsigned k = a.size();
-    r.resize(k, 0);
-    c.resize(k, 0);
-
-    for(unsigned i = 0; i < k; i++) {
-        double ci = r[i]/b[0];
-        c[i] += ci;
-        Poly bb = ci*b;
-        std::cout << ci <<"*" << b << ", r= " << r << std::endl;
-        r -= bb.shifted(i);
-    }
-    
-    return c;
 }
-*/
 
 Poly divide(Poly const &a, Poly const &b, Poly &r) {
     Poly c;
     r = a; // remainder
     assert(b.size() > 0);
-    
+
     const unsigned k = a.degree();
     const unsigned l = b.degree();
     c.resize(k, 0.);
-    
+
     for(unsigned i = k; i >= l; i--) {
         //assert(i >= 0);
         double ci = r.back()/b.back();
         c[i-l] += ci;
         Poly bb = ci*b;
-        //std::cout << ci <<"*(" << b.shifted(i-l) << ") = " 
-        //          << bb.shifted(i-l) << "     r= " << r << std::endl;
         r -= bb.shifted(i-l);
         r.pop_back();
     }
-    //std::cout << "r= " << r << std::endl;
     r.normalize();
     c.normalize();
-    
+
     return c;
 }
 
